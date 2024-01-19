89cee7c4b00eee4765bb004f6905040485c1c9a7
Sergey <PerelomaSV@gmail.com>
2024-01-19 19:11:40 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-import sys
-import time
-
 def main():
-    print("This is standard output (stdout).")
-    print("Starting the count...")
+    result = 10 / 0
+    print(f"Result: {result}")
 
-    for i in range(1, 11):
-        print(f"Count: {i}")
-        time.sleep(5)
-
-    try:
-        result = 10 / 0
-        print(f"Result: {result}")
-    except Exception as e:
-        print(f"An error occurred: {e}", file=sys.stderr)
 if __name__ == "__main__":
     main()
