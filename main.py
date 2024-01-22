6466d4b7cbe6b92b680ead92ed80b936f13fe2f9
Sergey <PerelomaSV@gmail.com>
2024-01-22 10:49:04 +0400

    Will fail

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-import sys
-import time
-
 def main():
-    print("Starting the count...")
-
-    for i in range(1, 11):
-        print(f"Count: {i}")
-        time.sleep(3)
-
-    try:
-        result = 10 / 0
-        print(f"Result: {result}")
-    except Exception as e:
-        print(f"An error occurred: {e}", file=sys.stderr)
+    result = 10 / 0
+    print(f"Result: {result}")
 
 if __name__ == "__main__":
     main()
